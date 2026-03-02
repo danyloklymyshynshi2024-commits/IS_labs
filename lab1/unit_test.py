633d49ccfd96ce26539e1fb3eb4bc488e590652e
travis <danylo.klymyshyn.shi.2024@lpnu.ua>
2026-03-02 13:53:24 +0000

    added comparison func for lemer algo and rand algo from lib

--- lab1/unit_test.py
+++ lab1/unit_test.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, random
 from back_end import generateNumbers, runTest
 
 class TestCryptoLogic(unittest.TestCase):
@@ -19,5 +19,25 @@ class TestCryptoLogic(unittest.TestCase):
         pi_val = runTest(numbers)
         self.assertTrue(2.0 <= float(pi_val) <= 4.0, f"Значення Pi ({pi_val}) занадто дивне")
 
+
+class CompareTwoFunctionsOnEfficiency:
+    def test_lemer_algorithm_w_random_library(self):
+        numbersLemer = generateNumbers(20000)
+        numbersRandom = [random.randint(1, 2**12-1) for _ in range(20000)]
+
+        pi_val_lemer = runTest(numbersLemer)
+        pi_val_random = runTest(numbersRandom)
+
+        return pi_val_lemer, pi_val_random
+
+
+
 if __name__ == '__main__':
+
+    obj = CompareTwoFunctionsOnEfficiency()
+    lemer_results, random_results = obj.test_lemer_algorithm_w_random_library()
+
+    print(f"Approximate pi value by Lemer algorithm: {lemer_results}")
+    print(f"Approximate pi value by random algorithm: {random_results}")
+
     unittest.main()
